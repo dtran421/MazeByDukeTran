31be83b0a9fe47e8dbc8da75cb382ffaafe62b4f
DukeTranWM <dtran@email.wm.edu>
2020-09-27 17:20:43 -0400

    imported interface files, created classes for ReliableRobot, Wizard, and ReliableSensor and created their CRCs

---
+++ MazeByDukeTran/src/gui/ReliableRobot.java
@@ -0,0 +1,131 @@
+package gui;
+
+import generation.CardinalDirection;
+
+/**
+ * @author Duke Tran
+ * Class: ReliableRobot
+ * <p>
+ * Responsibilites: perform move and rotate operations, interact with its Sensors,
+ * monitor and manage its energy consumption
+ * <p>
+ * Collaborators: Controller, RobotDriver (Wall-follower and Wizard), DistanceSensor (ReliableSensor and UnreliableSensor)
+ */
+public class ReliableRobot implements Robot {
+
+	@Override
+	public void setController(Controller controller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int[] getCurrentPosition() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CardinalDirection getCurrentDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getBatteryLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setBatteryLevel(float level) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getEnergyForFullRotation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getEnergyForStepForward() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOdometerReading() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void resetOdometer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rotate(Turn turn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void move(int distance) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isAtExit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInsideRoom() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasStopped() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int distanceToObstacle(Direction direction) throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean canSeeThroughTheExitIntoEternity(Direction direction) throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void startFailureAndRepairProcess(Direction direction, int meanTimeBetweenFailures, int meanTimeToRepair)
+			throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopFailureAndRepairProcess(Direction direction) throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MazeByDukeTran/src/gui/ReliableSensor.java
@@ -0,0 +1,56 @@
+package gui;
+
+import generation.CardinalDirection;
+import generation.Maze;
+import gui.Robot.Direction;
+
+/**
+ * @author Duke Tran
+ * Class: ReliableSensor
+ * <p>
+ * Responsibilites: sense distance to obstacle (wall), obtain energy cost for sensing,
+ * determine when a failure occurs and start and stop the repair process
+ * <p>
+ * Collaborators: Maze, RobotDriver (Wall-follower and Wizard), Robot (ReliableRobot)
+ */
+public class ReliableSensor implements DistanceSensor {
+
+	@Override
+	public int distanceToObstacle(int[] currentPosition, CardinalDirection currentDirection, float[] powersupply)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setMaze(Maze maze) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSensorDirection(Direction mountedDirection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getEnergyConsumptionForSensing() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startFailureAndRepairProcess(int meanTimeBetweenFailures, int meanTimeToRepair)
+			throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopFailureAndRepairProcess() throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
